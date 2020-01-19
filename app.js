18a2c8319d1d28f659df37125da9f3734d9ab80a
Alex Forsythe <awforsythe@gmail.com>
2020-01-18 21:03:32 -0600

    Add interactive canvas component: you can click to draw squares

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res, next) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 module.exports = app;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const size = 8;
+
+    const ctx = this.ref.current.getContext('2d');
+    ctx.fillStyle = '#ccff66';
+    ctx.fillRect(x - (size / 2), y - (size / 2), size, size);
+  };
+
+  render() {
+    const { width, height } = this.props;
+    return (
+      <canvas
+        ref={this.ref}
+        onClick={this.handleClick}
+        width={width}
+        height={height}
+        style={{ border: '1px solid #ccc' }}
+      />
+    );
+  }
+}
+Canvas.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+};
 
 const App = () => (
-  <h1>Hello</h1>
+  <Canvas width={800} height={600} />
 );
 
 ReactDOM.render(<App />, document.querySelector('#main'));
