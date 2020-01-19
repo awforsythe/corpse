84ea40d70a34cb504d2dd26dc59096c93eb4148e
Alex Forsythe <awforsythe@gmail.com>
2020-01-18 20:34:41 -0600

    Add basic React frontend with build environment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res, next) => {
-  res.send('OK whatever');
+  res.render('index');
 });
 
 module.exports = app;
